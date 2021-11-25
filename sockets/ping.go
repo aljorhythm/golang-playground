fbf2d227cde3b5259abca5c139067af5f3bf04dc
Joel Lim <103879u@gmail.com>
2021-11-26 03:34:07 +0800

    add get message

--- sockets/ping.go
+++ sockets/ping.go
@@ -2,12 +2,31 @@ package sockets
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
 	"html/template"
+	"log"
 	"net/http"
 )
 
+type PingStore struct {
+	messages []string
+}
+
+func (store *PingStore) addMessage(message string) {
+	store.messages = append(store.messages, message)
+}
+
+func NewStore() *PingStore {
+	return &PingStore{[]string{}}
+}
+
 type PingServer struct {
 	http.Handler
+	store *PingStore
+}
+
+func (s *PingServer) getStore() *PingStore {
+	return s.store
 }
 
 func (s *PingServer) pingHandler(writer http.ResponseWriter, request *http.Request) {
@@ -23,14 +42,29 @@ func (p *PingServer) indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
+func (p *PingServer) webSocket(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	conn ,err:= upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Panicf("webSocket %#v", err)
+	}
+	_, msg, _ := conn.ReadMessage()
+	p.getStore().addMessage(string(msg))
+}
+
 const jsonContentType = "application/json"
 
 func NewPingServer() *PingServer {
 	p := new(PingServer)
-
+	p.store = NewStore()
 	router := http.NewServeMux()
 	router.Handle("/ping", http.HandlerFunc(p.pingHandler))
 	router.Handle("/index", http.HandlerFunc(p.indexHandler))
+	router.Handle("/ws", http.HandlerFunc(p.webSocket))
 
 	p.Handler = router
 
--- sockets/ping_test.go
+++ sockets/ping_test.go
@@ -1,10 +1,13 @@
 package sockets
 
 import (
+	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestGame(t *testing.T) {
@@ -22,11 +25,35 @@ func TestGame(t *testing.T) {
 	t.Run("GET /index returns 200", func(t *testing.T) {
 		server := NewPingServer()
 
-		request, _ := http.NewRequest(http.MethodGet, "/ping", nil)
+		request, _ := http.NewRequest(http.MethodGet, "/index", nil)
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
 		assert.Equal(t, response.Code, http.StatusOK)
 	})
+
+	t.Run("when we get a message over a websocket should be good", func(t *testing.T) {
+		pingServer := NewPingServer()
+		server := httptest.NewServer(pingServer)
+		defer server.Close()
+
+		wsURL := "ws" + strings.TrimPrefix(server.URL, "http") + "/ws"
+		ws, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+		if err != nil {
+			t.Fatalf("could not open a ws connection on %s %v", wsURL, err)
+		}
+		defer ws.Close()
+
+		message := "ping!"
+		if err := ws.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
+			t.Fatalf("could not send message over ws connection %v", err)
+		}
+
+		time.Sleep(10 * time.Millisecond)
+
+		store :=  pingServer.getStore()
+		assert.Equal(t, []string{message},store.messages)
+	})
 }
+
